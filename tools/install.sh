01058020c7b85e943cb346228c4cfd2f0c330160
Junichi Kakisako <kilo.sw@gmail.com>
2020-02-27 21:31:51 +0900

    Updated

--- tools/install.sh
+++ tools/install.sh
@@ -3,33 +3,65 @@
 set -e
 
 # Default settings
-DOTFILES=${DOTFILES:-~/.dotfiles}
+DOTFILES=${DOTFILES:-~/.dotfiles.develop}
 REPO=${REPO:-kironono/dotfiles}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
-BRANCH=${BRANCH:-master}
+BRANCH=${BRANCH:-develop}
 
 
-command_exists() {
-    command -v "$@" >/dev/null 2>&1
+setup_color() {
+	# Only use colors if connected to a terminal
+	if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+	else
+		RED=""
+		GREEN=""
+		YELLOW=""
+		BLUE=""
+		BOLD=""
+		RESET=""
+	fi
 }
 
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+	echo ${RED}"Error: $@"${RESET} >&2
+}
+
+install_dotfiles() {
+	umask g-w,o-w
+
+	command_exists git || {
+		error "git is not installed"
+			exit 1
+		}
+
+	git clone --branch "$BRANCH" "$REMOTE" "$DOTFILES" || {
+		error "git clone of $REMOTE repo failed"
+			exit 1
+		}
+}
 
 main() {
-    umask g-w,o-w
+	setup_color
 
-    command_exists git || {
-        error "git is not installed"
-            exit 1
-        }
+	if [ -d "$DOTFILES" ]; then
+		cat <<-EOF
+			${YELLOW}You already have dotfiles installed.${RESET}
+			You'll need to remove '$DOTFILES' if you want to reinstall.
+		EOF
+		exit 1
+	fi
 
-    git clone -c core.eol=lf -c core.autocrlf=false \
-        -c fsck.zeroPaddedFilemode=ignore \
-        -c fetch.fsck.zeroPaddedFilemode=ignore \
-        -c receive.fsck.zeroPaddedFilemode=ignore \
-        --depth=1 --branch "$BRANCH" "$REMOTE" "$DOTFILES" || {
-        error "git clone of $REMOTE repo failed"
-            exit 1
-        }
+	install_dotfiles
 }
 
 main "$@"
