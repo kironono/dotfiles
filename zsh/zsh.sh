dc2d424d60b3236379c4938698b46b4350f23724
Junichi Kakisako <kilo.sw@gmail.com>
2020-02-29 17:45:50 +0900

    Updated

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,36 +1,66 @@
+#
+# path
+#
+export PATH=$DOTFILES/bin:$PATH
+fpath=($DOTFILES/zsh/functions $fpath)
 
-# 履歴ファイルの保存先
+#
+# history
+#
 export HISTFILE=${HOME}/.zsh_history
-
-# メモリに保存される履歴の件数
 export HISTSIZE=1000
-
-# 履歴ファイルに保存される履歴の件数
 export SAVEHIST=100000
-
-# 重複を記録しない
 setopt HIST_IGNORE_DUPS
-
-# 開始と終了を記録
 setopt EXTENDED_HISTORY
 
+#
+# env
+#
+export EDITOR=vim
 
 # compinit
 autoload -Uz compinit
 compinit
 
-zstyle ':completion:*' menu select
-
-setopt correct
-
 # colors
 autoload -Uz colors
 colors
 
+zstyle ':completion:*' menu select
+setopt correct
+
+# vcs_info
+autoload -Uz vcs_info
+
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+#
 # alias
+#
 alias ls='ls -G'
 alias ll='ls -alF'
-
 alias pwdc='pwd | tr -d "\n" | pbcopy'
+
+#
+# prompt
+#
+# %b ブランチ情報
+# %a アクション名(mergeなど)
+# %c changes
+# %u uncommit
+setopt prompt_subst
+zstyle ':vcs_info:git:*' check-for-changes true #formats 設定項目で %c,%u が使用可
+zstyle ':vcs_info:git:*' stagedstr "%F{green}!" #commit されていないファイルがある
+zstyle ':vcs_info:git:*' unstagedstr "%F{magenta}+" #add されていないファイルがある
+zstyle ':vcs_info:*' formats "%F{cyan}%c%u(%b)%f" #通常
+zstyle ':vcs_info:*' actionformats '[%b|%a]' #rebase 途中,merge コンフリクト等 formats 外の表示
+
+# プロンプト表示直前に vcs_info 呼び出し
+precmd () { vcs_info }
+
+# プロンプト（左）
+PROMPT='%{$fg[green]%}[%n%{$fg[blue]%}@%m]%{$reset_color%}'
+PROMPT=$PROMPT' ${vcs_info_msg_0_}%{${fg[white]}%}%}$%{${reset_color}%} '
+
+# プロンプト（右）
+RPROMPT='%{${fg[blue]}%}[%~]%{${reset_color}%}'
