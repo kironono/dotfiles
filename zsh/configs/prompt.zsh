f2b80ad13c1ff32d0f79e3a6bad5610c5b623503
Junichi Kakisako <kilo.sw@gmail.com>
2020-05-08 16:15:36 +0900

    Updated

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -13,6 +13,25 @@ zstyle ':vcs_info:git:*' unstagedstr "%F{magenta}+"
 zstyle ':vcs_info:*' formats "%F{cyan}%c%u(%b)%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 
+zstyle ':vcs_info:git*+set-message:*' hooks git-st
+function +vi-git-st() {
+    local ahead behind
+    local -a gitstatus
+
+    # for git prior to 1.7
+    # ahead=$(git rev-list origin/${hook_com[branch]}..HEAD | wc -l)
+    ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
+    (( $ahead )) && gitstatus+=( "+${ahead}" )
+
+    # for git prior to 1.7
+    # behind=$(git rev-list HEAD..origin/${hook_com[branch]} | wc -l)
+    behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
+    (( $behind )) && gitstatus+=( "-${behind}" )
+
+    hook_com[misc]+=${(j:/:)gitstatus}
+}
+
+
 precmd () { vcs_info }
 
 # prompt left
