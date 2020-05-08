c118e48ec983cdf4342f76d2787ed2cd7bf84816
Junichi Kakisako <kilo.sw@gmail.com>
2020-05-08 16:04:54 +0900

    Updated

--- zsh/configs/environment.zsh
+++ zsh/configs/environment.zsh
@@ -6,9 +6,28 @@ fi
 
 # node
 if [ -d "$HOME/.nvm" ]; then
-	export NVM_DIR="$HOME/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+	lazynvm() {
+		unset -f nvm node npm npx
+		export NVM_DIR="$HOME/.nvm"
+		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+	}
+	nvm() {
+		lazynvm
+		nvm $@
+	}
+	node() {
+		lazynvm
+		node $@
+	}
+	npm() {
+		lazynvm
+		npm $@
+	}
+	npx() {
+		lazynvm
+		npx $@
+	}
 fi
 
 # python
