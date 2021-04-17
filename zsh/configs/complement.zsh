fe265fd7b4435039dca6713cbe3dda85921aa911
Junichi Kakisako <kilo.sw@gmail.com>
2021-04-17 14:17:01 +0900

    Added twilio cli complement

--- zsh/configs/complement.zsh
+++ zsh/configs/complement.zsh
@@ -23,3 +23,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 zstyle ':completion:*' menu select
 # }}}
+
+# twilio autocomplete setup
+TWILIO_AC_ZSH_SETUP_PATH="${HOME}/.twilio-cli/autocomplete/zsh_setup" && test -f $TWILIO_AC_ZSH_SETUP_PATH && source $TWILIO_AC_ZSH_SETUP_PATH;
