d13ef2c35ae397d93bb921a67f8f2b414f38ecb8
Junichi Kakisako <kilo.sw@gmail.com>
2021-09-30 00:17:30 +0900

    added git-gh-diff-url installer

---
+++ .tools/installers/cl-tools/git-gh-diff-url/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ue
+
+SRC_DIR=$HOME/.local/src
+BIN_DIR=$HOME/.bin
+
+version=1.2.0
+archi=$(uname -sm)
+
+
+download() {
+    echo "Downloading ..."
+    pushd ${SRC_DIR}
+    curl -fL https://github.com/magicdrive/git-gh-diff-url/archive/refs/tags/v${version}.tar.gz | tar -xzf -
+    pushd "git-gh-diff-url-${version}"
+    cp Makefile Makefile.back
+    sed -i.bak "s|PREFIX=\${HOME}/local|PREFIX=${HOME}/.local|" Makefile
+    make install
+}
+
+
+case "$archi" in
+    Darwin\ *64)     download ;;
+    Linux\ *64)      download ;;
+    *)               echo "not supported!" && exit 1 ;;
+esac
